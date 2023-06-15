0601df6f46762243f1a23ed828aaf7658617c9c9
Priyanshu <priyanshu0102here@gmail.com>
2023-06-15 20:16:44 +0530

    Updated!

--- app.js
+++ app.js
@@ -15,17 +15,17 @@ app.use("/files",express.static("./public/files"));
 app.use(router);
 
 // Your code
-if (process.env.NODE_ENV === "production") {
-    const path = require("path");
-    app.use(express.static(path.resolve(__dirname, 'client', 'build')));
-    app.get("/", (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'),function (err) {
-            if(err) {
-                res.status(500).send(err)
-            }
-        });
-    })
-}
+// if (process.env.NODE_ENV === "production") {
+//     const path = require("path");
+//     app.use(express.static(path.resolve(__dirname, 'client', 'build')));
+//     app.get("/", (req, res) => {
+//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'),function (err) {
+//             if(err) {
+//                 res.status(500).send(err)
+//             }
+//         });
+//     })
+// }
 // Your code
 
 
